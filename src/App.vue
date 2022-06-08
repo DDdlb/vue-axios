3b81390daad9141c216ecd7bd75e11d72214564a
DDlb <1032191254@qq.com>
2022-06-08 23:20:37 +0800

    axios封装

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
+  <h1>{{count}}</h1>
+  <button @click="handleAdd">+</button>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
+
+
+<script setup name="app">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+import http from './utils/http'
+
+let count = ref(0)
+
+const handleAdd = ()=>{
+  count.value = count.value + 1
+  console.log(count.value);
+}
+
+onMounted(()=>{
+  console.log("mounted");
+  http.get('/api/test').then((res)=>{
+    console.log(res);
+  })
+})
+
+</script>
 
 <style>
 #app {
